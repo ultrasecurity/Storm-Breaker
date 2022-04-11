e1d9e5711d94eeda4e5f783c20f4fc2d3936f491
uidops <uidops@protonmail.com>
2022-04-11 11:08:08 +0430

    add Gentoo to installer

--- install.sh
+++ install.sh
@@ -16,4 +16,8 @@ checkroot
 KERNEL="$(uname -s | tr '[:upper:]' '[:lower:]')"
 if [ "$KERNEL" = "linux" ]; then
     DISTRO="$(grep ^NAME= /etc/os-release | cut -d= -f2 | tr '[:upper:]' '[:lower:]')"
+    if [ "$DISTRO" = "gentoo" ]; then
+        emerge --sync
+        emerge -av dev-lang/php dev-python/pip app-misc/neofetch
+    fi
 fi
