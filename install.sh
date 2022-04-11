dea04147915820fe1a1852ecf4ca744d0086b48d
uidops <uidops@protonmail.com>
2022-04-11 11:18:09 +0430

    use ID instead of NAME for distro detection

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ apt_based() {
 
 KERNEL="$(uname -s | tr '[:upper:]' '[:lower:]')"
 if [ "$KERNEL" = "linux" ]; then
-    DISTRO="$(grep ^NAME= /etc/os-release | cut -d= -f2 | tr '[:upper:]' '[:lower:]')"
+    DISTRO="$(grep ^ID= /etc/os-release | cut -d= -f2 | tr '[:upper:]' '[:lower:]')"
     if [ "$DISTRO" = "gentoo" ]; then
         emerge --sync
         emerge -av dev-lang/php dev-python/pip app-misc/neofetch
