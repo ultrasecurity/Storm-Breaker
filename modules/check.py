aaaecd9a51b284ff8d2d38d8660c135593b6ced2
Peppe289 <gsperanza204@gmail.com>
2023-08-18 13:40:18 +0200

    modules: check: add auto update
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- modules/check.py
+++ modules/check.py
@@ -1,9 +1,10 @@
-from subprocess import getoutput
+import subprocess
 import requests,json
 from modules import control
+import os
 
 def dependency():
-    check_php = getoutput("php -v")
+    check_php = subprocess.getoutput("php -v")
     if "not found" in check_php:
         exit("please install php \n command > sudo apt install php")
 
@@ -30,7 +31,45 @@ def check_started():
         control.kill_php_proc()
 
 
+def git_update():
+    check_git = subprocess.getoutput("git -v")
+    if "not found" in check_git:
+        exit("Please install git")
 
+    path = "./.git"
+    isExist = os.path.exists(path)
+    if isExist == False:
+        exit("For the automatic update, download repository with git")
+
+    # Get the latest commit hash. necessary for the reset
+    git_head = subprocess.getoutput("git rev-parse HEAD")
+    confirm = input("This will reset all uncommitted changes with git and it will sync with the official repository.\nConfirm? [N/y]: ")
+    if confirm == 'Y' or confirm == 'y':
+
+        # Reset at runtime is required. The storm-web/Settings.json file changes the value of "is_start" from false to true.
+        # This causes a conflict and we simply won't be able to update for this automatic change.
+        result = subprocess.Popen(["git reset --hard", str(git_head)], shell = True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        result.wait()
+        err=result.returncode
+
+        if err != 0:
+            exit("Some error with reset")
+
+        result = subprocess.Popen("git fetch https://github.com/ultrasecurity/Storm-Breaker", shell = True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        result.wait()
+        err=result.returncode
+
+        if err != 0:
+            exit("Some error with fetch")
+
+        result = subprocess.Popen("git merge FETCH_HEAD --no-edit", shell = True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        result.wait()
+        err=result.returncode
+
+        if err != 0:
+            exit("Some error with merge")
+
+        exit("Update done. Start the program again!")
 
 def check_update():
     http = requests.get("https://raw.githubusercontent.com/ultrasecurity/Storm-Breaker/main/Settings.json").text
@@ -41,4 +80,8 @@ def check_update():
 
         data = json.load(jsonFile)
         if data['version'] < http_json['version']:
-            exit("Please Update Tool")
+            update = input("Do you want to try to update automatically? [N/y]: ")
+            if update == 'Y' or update == 'y':
+                git_update()
+            else:
+                exit("Please Update Tool")
